995b7c31b990fd35a7b4ecf90305b085c663190c
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-30 21:47:23 +0530

    Update `V2.1`
    Changelogs :
    - Support for Odd Number of Accounts Switch Ranging from 1, 3, 5, 7, 9 . . . ( So, Now Any Number of Accounts are Supported )
    - Using itertools, Script is Efficient and Fast
    - Proper Usage, Visit Our Support Group !!
    ~ 5MysterySD

--- script_noloop.py
+++ script_noloop.py
@@ -4,23 +4,24 @@
 #
 # All Right Reserved 
 
-import time
-import os
+from itertools import zip_longest
+from time import sleep
+from os.environ import get as getVar
 from datetime import datetime
-import heroku3
+from heroku3 import from_key as from_apikey
 
 # Required vars >>>>>>>> Add Unlimited Number of Apps separated by space in each var
 # A = The Credentials of the First Account
 # B = The Credentials of the Second Account
 # C = The Credentials of the Third Account
-A_APPNAME = os.environ.get('A_APPNAME',"")
-A_APIKEY = os.environ.get('A_APIKEY',"")
-B_APPNAME = os.environ.get('B_APPNAME',"")
-B_APIKEY = os.environ.get('B_APIKEY',"")
-PROCESSTYPE = os.environ.get('PROCESSTYPE',"")
-CA_APPNAME = os.environ.get('CA_APPNAME',"")
-CB_APPNAME = os.environ.get('CB_APPNAME',"")
-C_APIKEY = os.environ.get('C_APIKEY',"")
+A_APPNAME = getVar('A_APPNAME',"")
+A_APIKEY = getVar('A_APIKEY',"")
+B_APPNAME = getVar('B_APPNAME',"")
+B_APIKEY = getVar('B_APIKEY',"")
+PROCESSTYPE = getVar('PROCESSTYPE',"")
+CA_APPNAME = getVar('CA_APPNAME',"")
+CB_APPNAME = getVar('CB_APPNAME',"")
+C_APIKEY = getVar('C_APIKEY',"")
 
 # List Forming >>>>>>>>
 A_APPNAME = A_APPNAME.split(" ")
@@ -37,77 +38,105 @@ today = datetime.now()
 
 # Multiple Pair of Apps >>>>>>>>
 print("Checking the Conditions & Vars for the app ..")
-if(len(PROCESSTYPE) != 0 and len(A_APIKEY) != 0 and len(A_APPNAME) != 0 and len(C_APIKEY) != 0 and len(CA_APPNAME) != 0 and len(CB_APPNAME) != 0 and len(B_APIKEY) != 0 and len(B_APPNAME) != 0):
+if(len(PROCESSTYPE) != 0 and len(A_APIKEY) != 0 and len(A_APPNAME) != 0 and len(C_APIKEY) != 0 and len(CA_APPNAME) != 0):
   if(today.day == 23):
-    for  (A, AAPP, PRO, B, BAPP, C, CAAPP, CBAPP) in zip(A_APIKEY, A_APPNAME, PROCESSTYPE, B_APIKEY, B_APPNAME, C_APIKEY, CA_APPNAME, CB_APPNAME):
+    for  (A, AAPP, PRO, B, BAPP, C, CAAPP, CBAPP) in zip_longest(A_APIKEY, A_APPNAME, PROCESSTYPE, B_APIKEY, B_APPNAME, C_APIKEY, CA_APPNAME, CB_APPNAME):
+      if len(CB_APPNAME) != 0 and len(B_APIKEY) != 0 and len(B_APPNAME) != 0:
+        print("Changing the Dyno of the First Account..")
+        heroku_conn = from_apikey(A)
+        app = heroku_conn.app(AAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The App in the First Account has been Scaled Down.")
 
-      print("Changing the Dyno of the First Account..")
-      heroku_conn = heroku3.from_key(A)
-      app = heroku_conn.app(AAPP)
-      app.process_formation()[PRO].scale(0)
-      print("The App in the First Account has been Scaled Down.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of the Second Account..")
+        heroku_conn = from_apikey(B)
+        app = heroku_conn.app(BAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The App in the Second Account has been Scaled Down.")
 
-      print("Changing the Dyno of the Second Account..")
-      heroku_conn = heroku3.from_key(B)
-      app = heroku_conn.app(BAPP)
-      app.process_formation()[PRO].scale(0)
-      print("The App in the Second Account has been Scaled Down.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of First App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CAAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The First App in the Third Account has been Scaled Up.")
 
-      print("Changing the Dyno of First App of the Third Account..")
-      heroku_conn = heroku3.from_key(C)
-      app = heroku_conn.app(CAAPP)
-      app.process_formation()[PRO].scale(1)
-      print("The First App in the Third Account has been Scaled Up.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of Second App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CBAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The Second App in the Third Account has been Scaled Up.")
+      else: # Single Account Switch Function
+        print("Changing the Dyno of the First Account..")
+        heroku_conn = from_apikey(A)
+        app = heroku_conn.app(AAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The App in the First Account has been Scaled Down.")
 
-      print("Changing the Dyno of Second App of the Third Account..")
-      heroku_conn = heroku3.from_key(C)
-      app = heroku_conn.app(CBAPP)
-      app.process_formation()[PRO].scale(1)
-      print("The Second App in the Third Account has been Scaled Up.")
+        sleep(5)
+
+        print("Changing the Dyno of First App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CAAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The First App in the Third Account has been Scaled Up.")
 
   elif(today.day == 1):
-    for  (A, AAPP, PRO, B, BAPP, C, CAAPP, CBAPP) in zip(A_APIKEY, A_APPNAME, PROCESSTYPE, B_APIKEY, B_APPNAME, C_APIKEY, CA_APPNAME, CB_APPNAME):
+    for  (A, AAPP, PRO, B, BAPP, C, CAAPP, CBAPP) in zip_longest(A_APIKEY, A_APPNAME, PROCESSTYPE, B_APIKEY, B_APPNAME, C_APIKEY, CA_APPNAME, CB_APPNAME):
+      if len(CB_APPNAME) != 0 and len(B_APIKEY) != 0 and len(B_APPNAME) != 0:
+        print("Changing the Dyno of First App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CAAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The First App in the Third Account has been Scaled Down.")
 
-      print("Changing the Dyno of First App of the Third Account..")
-      heroku_conn = heroku3.from_key(C)
-      app = heroku_conn.app(CAAPP)
-      app.process_formation()[PRO].scale(0)
-      print("The First App in the Third Account has been Scaled Down.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of Second App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CBAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The Second App in the Third Account has been Scaled Down.")
 
-      print("Changing the Dyno of Second App of the Third Account..")
-      heroku_conn = heroku3.from_key(C)
-      app = heroku_conn.app(CBAPP)
-      app.process_formation()[PRO].scale(0)
-      print("The Second App in the Third Account has been Scaled Down.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of the First Account..")
+        heroku_conn = from_apikey(A)
+        app = heroku_conn.app(AAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The App in the First Account has been Scaled Up.")
 
-      print("Changing the Dyno of the First Account..")
-      heroku_conn = heroku3.from_key(A)
-      app = heroku_conn.app(AAPP)
-      app.process_formation()[PRO].scale(1)
-      print("The App in the First Account has been Scaled Up.")
+        sleep(5)
 
-      time.sleep(5)
+        print("Changing the Dyno of the Second Account..")
+        heroku_conn = from_apikey(B)
+        app = heroku_conn.app(BAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The App in the Second Account has been Scaled Up.")
+      else: # Single Account Switch Function
+        print("Changing the Dyno of First App of the Third Account..")
+        heroku_conn = from_apikey(C)
+        app = heroku_conn.app(CAAPP)
+        app.process_formation()[PRO].scale(0)
+        print("The First App in the Third Account has been Scaled Down.")
 
-      print("Changing the Dyno of the Second Account..")
-      heroku_conn = heroku3.from_key(B)
-      app = heroku_conn.app(BAPP)
-      app.process_formation()[PRO].scale(1)
-      print("The App in the Second Account has been Scaled Up.")
+        sleep(5)
+
+        print("Changing the Dyno of the First Account..")
+        heroku_conn = from_apikey(A)
+        app = heroku_conn.app(AAPP)
+        app.process_formation()[PRO].scale(1)
+        print("The App in the First Account has been Scaled Up.")
 
   else:
-    print("Today is not 1st or 23rd of Month")
+    print("Today is not 1st or 23rd of Current Month")
 else:
-  print("Variables for the app are not fully filled. RECHECK AGAIN.")
+  print("Variables for the app are not fully filled. RECHECK AGAIN. ")
      
 # Ending the Current process >>>>>>>>
 print("\nThe Script has been Executed.")
